367455b84a6aa890a7af5be3d40d13c45eb8f237
caufieldjh <j.harry.caufield@gmail.com>
2024-04-22 16:35:05 -0400

    Linting

--- src/clive/loaders/sssom_loader.py
+++ src/clive/loaders/sssom_loader.py
@@ -9,7 +9,8 @@ from sssom.util import MappingSetDataFrame
 def load_map_file(input_path: Path) -> MappingSetDataFrame:
     """Load a single local SSSOM TSV or other SSSOM-compatible format.
 
-    :param  input_path: The path to the input file in one of the legal formats, eg obographs, aligmentapi-xml
+    :param  input_path: The path to the input file in one of the legal
+      formats, eg obographs, aligmentapi-xml
     :return: A MappingSetDataFrame object.
     """
     msdf = parse_sssom_table(input_path)
