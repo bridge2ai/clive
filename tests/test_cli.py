5f94f1d99de6a02d069643337baa45c26cc08220
caufieldjh <j.harry.caufield@gmail.com>
2024-04-22 16:05:10 -0400

    Add simple tests

---
+++ tests/test_cli.py
@@ -0,0 +1,19 @@
+"""Tests for the CLI."""
+
+import unittest
+
+from click.testing import CliRunner, Result
+
+from clive.cli import main
+
+class TestCommandLineInterface(unittest.TestCase):
+    """Tests all command-line subcommands."""
+
+    def setUp(self) -> None:
+        runner = CliRunner(mix_stderr=False)
+        self.runner = runner
+    
+    def test_main_help(self):
+        result = self.runner.invoke(main, ["--help"])
+        out = result.stdout
+        self.assertEqual(0, result.exit_code)
