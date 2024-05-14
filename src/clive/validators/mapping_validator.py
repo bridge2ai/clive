350d29fa1a387154339069441f0dff28e3a1c589
caufieldjh <j.harry.caufield@gmail.com>
2024-05-14 16:55:24 -0400

    Expand validation

--- src/clive/validators/mapping_validator.py
+++ src/clive/validators/mapping_validator.py
@@ -2,9 +2,25 @@
 
 import logging
 
-from sssom.validators import check_all_prefixes_in_curie_map
+from jsonschema import ValidationError
+from sssom.validators import check_all_prefixes_in_curie_map, validate_json_schema
+
 
 def validate_map(msdf):
     """Validate a SSSOM map."""
+
+    # TODO: refactor this a bit
+
     logging.info("Checking prefixes in CURIE map.")
-    check_all_prefixes_in_curie_map(msdf)
+    try:
+        check_all_prefixes_in_curie_map(msdf)
+    except ValidationError as e:
+        logging.error(f"Validation error when checking CURIE map prefixes: {e}")
+        raise e
+    
+    logging.info("Checking against SSSOM schema.")
+    try:
+        validate_json_schema(msdf)
+    except ValidationError as e:
+        logging.error(f"Validation error when validating against SSSOM schema: {e}")
+        raise e
